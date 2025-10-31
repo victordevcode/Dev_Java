aa022e9e0bc75dc6f232cf4677db0a7bfe0461da
Victor Hugo <victorhugosant7@gmail.com>
2025-10-31 18:50:49 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main (String[] args) {
+        JFrame janela= new JFrame();
+        janela.setTitle("Meu primeiro Programa!");
+        janela.setSize(500,500);
+        janela.setVisible(true);
+    }
+}
